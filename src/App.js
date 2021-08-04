38af80235692413b30b0ab95418d6b71e882cc79
alfarotama <alfarohadiyantama13@gmail.com>
2021-08-04 13:02:51 +0700

    recovering data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Homepage from "./pages/Homepage";
+import CreatePlaylist from "./pages/CreatePlaylist";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function App() {
-	return <div className="App"></div>;
+	const { isAuthenticated } = useSelector((state) => state.auth);
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Navbar />
+				<main>
+					<Sidebar />
+					<Switch>
+						<Route path="/create-playlist">
+							{isAuthenticated ? <CreatePlaylist /> : <Redirect to="/" />}
+						</Route>
+						<Route path="/">
+							<Homepage />
+						</Route>
+					</Switch>
+				</main>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
