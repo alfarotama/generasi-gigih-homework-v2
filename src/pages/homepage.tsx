7721d61d427304ef721c49077654f4a0becb3568
alfarotama <alfarohadiyantama13@gmail.com>
2021-08-14 15:53:43 +0700

    trying to fix client id on vercel

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -7,7 +7,7 @@ const Homepage = () => {
 	return (
 		<Center bg="trueGray.800" borderRadius={4}>
 			{isAuthenticated ? (
-				<Text>You Already Login</Text>
+				<Text>You Already Login !</Text>
 			) : (
 				<Text>You Can Login By clicking The Login Button</Text>
 			)}
